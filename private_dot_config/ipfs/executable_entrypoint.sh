3bf1c060e20a68e02f8e3478421e6edda390a57c
budybye <101wayout@gmail.com>
2024-11-02 13:31:11 +0900

    seiri

--- private_dot_config/ipfs/executable_entrypoint.sh
+++ private_dot_config/ipfs/executable_entrypoint.sh
@@ -2,7 +2,7 @@
 set -eu
 
 # IPFS_PATHの設定ファイルのパス設定
-# export IPFS_PATH=${HOME}.config/.ipfs
+# export IPFS_PATH=${HOME}.config/ipfs
 
 # # IPFSの初期化 (既に初期化されている場合はスキップ)
 if [ ! -f "$IPFS_PATH/config" ]; then
@@ -17,17 +17,4 @@ ipfs add "/export" -r --cid-version=1
 # CID=$(ipfs add "/export" -r --cid-version=1 | awk '{print $2}' | head -n 1)
 # ipfs name publish ${CID} --allow-offline
 
-
-# PrivateなPeerに設定する場合
-# export PRIVATE_PEER_IP=
-# export PRIVATE_PEER_IP_ADDR=
-
-# 既存のbootstrapノードを全て削除
-# ipfs bootstrap rm all
-
-# 新しいbootstrapノードを追加
-# この部分は環境変数PRIVATE_PEER_IP_ADDRとPRIVATE_PEER_IDが設定されていることを前提としています
-# ipfs bootstrap add "/ip4/$PRIVATE_PEER_IP_ADDR/tcp/4001/ipfs/$PRIVATE_PEER_ID"
-
-# IPFSデーモンの起動
 ipfs daemon
