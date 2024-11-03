1986815e163765b5f5ac32d7b065440c1e5457a4
budybye <101wayout@gmail.com>
2024-11-03 14:20:16 +0900

    ..

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -4,7 +4,7 @@
 sudo pmset -a standbydelay 86400
 
 # xcode commond tool install
-xcode-select --install && sudo xcode-select --switch /Library/Developer/CommandLineTools
+# xcode-select --install && sudo xcode-select --switch /Library/Developer/CommandLineTools
 
 # brewコマンドが実行可能な場合evalを実行する
 # homebrew をインストールされていなければインストールする
