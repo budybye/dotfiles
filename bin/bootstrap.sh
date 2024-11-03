030c587c5d3eaa9996c0e96da03cdd7f33f97f01
budybye <101wayout@gmail.com>
2024-11-03 14:24:59 +0900

    b

--- bin/bootstrap.sh
+++ bin/bootstrap.sh
@@ -1,26 +1,28 @@
-#!/bin/sh -ex
+#!/bin/sh
+
+# set -ex
 
 ## スリープ状態に移行するまでの時間，単位は秒(24時間)
 sudo pmset -a standbydelay 86400
 
 # xcode commond tool install
-# xcode-select --install && sudo xcode-select --switch /Library/Developer/CommandLineTools
+xcode-select --install && sudo xcode-select --switch /Library/Developer/CommandLineTools
 
 # brewコマンドが実行可能な場合evalを実行する
 # homebrew をインストールされていなければインストールする
 # brewのPATHを通す
-if command -v brew > /dev/null; then
+if command -v brew >/dev/null; then
     eval "$(brew shellenv)"
 else
     echo "brew is not installed"
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    echo 'export PATH="/opt/homebrew/bin:$PATH"' >> "${HOME}/.zshenv"
-    . "${HOME}/.zshenv"
+    echo 'export PATH="/opt/homebrew/bin:$PATH"' >> ~/.zshenv
+    source ~/.zshenv
 fi
 
 # Brewfileからインストールする
-# brew tap Homebrew/bundle && brew bundle --file "${XDG_CONFIG_HOME}/Brewfile"
-brew tap Homebrew/bundle && brew bundle --file "${XDG_CONFIG_HOME}/Brewfile-test"
+# brew tap Homebrew/bundle && brew bundle --file '~/.config/Brewfile'
+brew tap Homebrew/bundle && brew bundle --file '~/.config/Brewfile-test'
 
 # Rosetta2 のインストール
 # softwareupdate --install-rosetta
