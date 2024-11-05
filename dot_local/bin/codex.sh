469e532c1559688afa433c4b8fb7c049286e2850
budybye <121966396+budybye@users.noreply.github.com>
2024-11-06 01:11:09 +0900

    sudo

--- dot_local/bin/codex.sh
+++ dot_local/bin/codex.sh
@@ -8,24 +8,24 @@ CODE_DIR="${XDG_CONFIG_HOME}/vscode"
 # CODE_DIR="${XDG_CONFIG_HOME}/Cursor"
 
 # VSCodeのUserディレクトリの作成
-mkdir -p "${CODE_DIR}/User"
+sudo mkdir -p "${CODE_DIR}/User"
 
 extensions_json() {
     # extensions.jsonのパス
     CODEX="${CODE_DIR}/extensions.json"
     # extensions.jsonが存在する場合はバックアップを作成
     if [ -f "${CODEX}" ]; then
-        cp "${CODEX}" "${CODEX}.copy"
+        sudo cp "${CODEX}" "${CODEX}.copy"
         echo "バックアップを作成しました: ${CODEX}.copy"
     fi
 
     if ! command -v code >/dev/null 1>&2; then
         echo "VScodeが存在しません。"
-        cp "${XDG_DATA_HOME}/vscode/extensions.json" "${CODEX}"
+        sudo cp "${XDG_DATA_HOME}/vscode/extensions.json" "${CODEX}"
     else
         # インストールされている拡張機能を"extensions.json"の形式で出力する
-        echo '{"recommendations": [],"unwantedRecommendations":[]}' >"${CODEX}"
-        code --list-extensions | xargs -I {} sh -c "jq '.recommendations += [\"{}\"]' ${CODEX} > temp && mv temp ${CODEX}"
+        sudo echo '{"recommendations": [],"unwantedRecommendations":[]}' >"${CODEX}"
+        sudo code --list-extensions | xargs -I {} sh -c "jq '.recommendations += [\"{}\"]' ${CODEX} > temp && mv temp ${CODEX}"
         echo '拡張機能の更新が完了しました!'
     fi
     cat "${CODEX}"
@@ -36,12 +36,12 @@ settings_json() {
     SETTINGS_JSON="${CODE_DIR}/User/settings.json"
     # settings.jsonが存在する場合はバックアップを作成
     if [ -f "${SETTINGS_JSON}" ]; then
-        cp "${SETTINGS_JSON}" "${SETTINGS_JSON}.copy"
+        sudo cp "${SETTINGS_JSON}" "${SETTINGS_JSON}.copy"
         echo "バックアップを作成しました: ${SETTINGS_JSON}.copy"
     else
         echo "${SETTING_JSON}が存在しません。"
-        mkdir -p "$(dirname "${SETTINGS_JSON}")"
-        cp "${XGD_DATA_HOME}/vscode/user-data/User/settings.json" "${SETTINGS_JSON}"
+        sudo mkdir -p "$(dirname "${SETTINGS_JSON}")"
+        sudo cp "${XGD_DATA_HOME}/vscode/user-data/User/settings.json" "${SETTINGS_JSON}"
         echo "新しい${SETTING_JSON}を作成しました。"
     fi
     cat "${SETTINGS_JSON}"
@@ -56,8 +56,8 @@ keybindings_json() {
         echo "バックアップを作成しました: ${KEYBINDINGS_JSON}.copy"
     else
         echo "${SEKEYBINDINGS_JSONTTING_JSON}が存在しません。"
-        mkdir -p "$(dirname "${KEYBINDINGS_JSON}")"
-        cp "${XGD_DATA_HOME}/vscode/user-data/User/keybindings.json" "${KEYBINDINGS_JSON}"
+        sudo mkdir -p "$(dirname "${KEYBINDINGS_JSON}")"
+        sudo cp "${XGD_DATA_HOME}/vscode/user-data/User/keybindings.json" "${KEYBINDINGS_JSON}"
         echo "新しい${KEYBINDINGS_JSON}を作成しました。"
     fi
     cat "${KEYBINDINGS_JSON}"
