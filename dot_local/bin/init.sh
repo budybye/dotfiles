2ce89b601c3dc24093e811a0a95f7a8103f5de41
budybye <121966396+budybye@users.noreply.github.com>
2024-11-05 23:40:24 +0900

    Update init.sh

--- dot_local/bin/init.sh
+++ dot_local/bin/init.sh
@@ -3,14 +3,17 @@ set -ex
 
 GIT_USER="${GIT_AUTHOR_NAME:-budybye}"
 
-pwd
-ls -la
-sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply ${GIT_USER} || {
-    echo "### chezmoi のインストールに失敗しました。"
-    exit 1
-}
+if ! command -v chezmoi >/dev/null 2>&1; then
+    sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply ${GIT_USER} || {
+        echo "### chezmoi のインストールに失敗しました。"
+        exit 1
+    }
+    "$(pwd)"/bin/chezmoi help
+else
+    echo "### chezmoi がすでにインストールされています。"
+    chezmoi init --apply ${GIT_USER}
+fi
 ls -la ${HOME}
 ls -la ${HOME}/.local/bin
 ls -la ${XDG_CONFIG_HOME}
 ls -la ${XDG_DATA_HOME}
-"$(pwd)"/bin/chezmoi help
