4bcd7d2b7b6fa8d7cd7f9b521bd90cb85ba97aa0
budybye <121966396+budybye@users.noreply.github.com>
2024-11-06 01:55:08 +0900

    Update init.sh

--- dot_local/bin/init.sh
+++ dot_local/bin/init.sh
@@ -1,19 +1,20 @@
 #!/usr/bin/env bash
 set -ex
 
-GIT_USER="${GIT_AUTHOR_NAME:-budybye}"
+GIT_USER="${GIT_AUTHOR_NAME:--S .}"
 
 if ! command -v chezmoi >/dev/null 2>&1; then
-    sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply ${GIT_USER} || {
+    sh -c "$(curl -fsLS chezmoi.io/get)" -- init --apply "${GIT_USER}" || {
         echo "### chezmoi のインストールに失敗しました。"
         exit 1
     }
     "$(pwd)"/bin/chezmoi help
 else
     echo "### chezmoi がすでにインストールされています。"
-    chezmoi init --apply ${GIT_USER}
+    chezmoi init --apply "${GIT_USER}"
 fi
-ls -la ${HOME}
-ls -la ${HOME}/.local/bin
-ls -la ${XDG_CONFIG_HOME}
-ls -la ${XDG_DATA_HOME}
+ls -la "${HOME}"
+ls -la "${HOME}/.local/bin"
+ls -la "${XDG_CONFIG_HOME:-$HOME/.config}"
+ls -la "${XDG_DATA_HOME:-$HOME/.local/share}"
+
