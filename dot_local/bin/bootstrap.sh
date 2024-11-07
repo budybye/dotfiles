4dbec9b1beb08d9734a5f306cb9938ff713f75fa
${GIT_AUTHOR_NAME} <${GIT_AUTHOR_EMAIL}>
2024-11-08 06:20:29 +0900

    git?

--- dot_local/bin/bootstrap.sh
+++ dot_local/bin/bootstrap.sh
@@ -61,7 +61,6 @@ install_brew_packages() {
     if [ -f "$BREWFILE_PATH" ]; then
         echo "Brewfile ($BREWFILE_PATH) からパッケージをインストールします。"
         brew tap Homebrew/bundle
-        brew tap aquaproj/aqua
         brew bundle --file="$BREWFILE_PATH" || {
             echo "brew bundleのインストールが失敗しました。"
             exit 1
