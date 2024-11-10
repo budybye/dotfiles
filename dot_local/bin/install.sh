4528e2588f622fdde0bb175dab177663ac1ad2cc
budybye <101wayout@gmail.com>
2024-11-10 22:42:58 +0900

    tik

--- dot_local/bin/install.sh
+++ dot_local/bin/install.sh
@@ -35,7 +35,7 @@ install_packages() {
         libfuse2 libssl-dev pkg-config apt-transport-https ca-certificates lsb-release libnss3-tools \
         libinput-tools libdb-dev libdb5.3-dev libgdbm-dev libgmp-dev libgmpxx4ldbl libgdbm-compat-dev rustc \
         libstd-rust-1.75 libstd-rust-dev libncurses5-dev libffi-dev libfreetype6-dev libfontconfig1-dev libxcb-xfixes0-dev libxkbcommon-dev \
-        ffmpeg mpd mpc ncmpcpp net-tools nmap snapd ufw rsyslog im-config byobu ruby || {
+        ffmpeg mpd mpc ncmpcpp net-tools nmap snapd ufw rsyslog im-config byobu ruby cargo|| {
         echo "### apt のインストールに失敗しました。"
         exit 1
     }
@@ -133,7 +133,7 @@ install_mise() {
     mise activate zsh
     # mise activate --shims
 
-    mise use chezmoi bun starship node go rust -y || {
+    mise use chezmoi bun starship node go -y --verbose || {
         echo "### mise use に失敗しました。"
         exit 1
     }
