8ef4fc275df50d82212805cfc03fa2e72aaa85ea
budybye <101wayout@gmail.com>
2024-11-19 18:00:59 +0900

    ll

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 
-chezmoi init --apply budybye || su $(whoami) -c 'sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin" init --apply budybye'
+chezmoi init --apply budybye || sh -c "$(curl -fsLS get.chezmoi.io)" -- -b "$HOME/.local/bin" init --apply budybye
