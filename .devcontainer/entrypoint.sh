f2bc076abf28b970cd8e03c0d431e4c0c4f66902
--global <shallot96@gmail.com>
2024-12-03 00:39:48 +0900

    gg

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,19 +1,81 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# コンテナ起動時に /sbin/init を実行 systemd を使いたい
-# /sbin/init &
+# xrdp サービスを開始
+start_xrdp_services() {
+    # Preventing xrdp startup failure
+    rm -rf /var/run/xrdp-sesman.pid
+    rm -rf /var/run/xrdp.pid
+    rm -rf /var/run/xrdp/xrdp-sesman.pid
+    rm -rf /var/run/xrdp/xrdp.pid
 
-# デバッグ情報の出力（必要に応じて）
-echo "entrypoint.sh..."
-uname -a
-ls -la
-ls -la dotfiles
+    # Use exec ... to forward SIGNAL to child processes
+    xrdp-sesman && exec xrdp -n
+}
 
-echo "chezmoi init..."
-chezmoi init --help
+# error が発生した場合に xrdp サービスを停止
+stop_xrdp_services() {
+    xrdp --kill
+    xrdp-sesman --kill
+    exit 0
+}
 
-echo "env..."
-env
+# エントリーポイントスクリプトが実行されたことを通知
+echo Entryponit script is Running...
+echo
 
-# Dockerfileで指定されたCMDを実行
-exec "$@"
+# cmd の引数を3つ指定 ユーザー名 パスワード スーパーユーザーかどうか
+# デフォルトは CMD["ubuntu", "ubuntu", "no"]
+
+users=$(($#/3))
+mod=$(($# % 3))
+
+# echo "users is $users"
+# echo "mod is $mod"
+
+if [[ $# -eq 0 ]]; then
+    echo "No input parameters. exiting..."
+    echo "there should be 3 input parameters per user"
+    exit
+fi
+
+if [[ $mod -ne 0 ]]; then
+    echo "incorrect input. exiting..."
+    echo "there should be 3 input parameters per user"
+    exit
+fi
+echo "You entered $users users"
+
+# 引数をループしてユーザーを作成
+while [ $# -ne 0 ]; do
+
+    # グループを作成 ユーザー名と同じグループ名
+    addgroup $1
+    # echo "username is $1"
+    # useradd -m -s /bin/bash -g $1 $1
+    if [ $(command -v zsh) ]; then
+        useradd -m -s /usr/bin/zsh -g $1 $1
+    else
+        useradd -m -s /bin/bash -g $1 $1
+    fi
+
+    # パスワードを設定
+    # getent passwd | grep $1
+    echo $1:$2 | chpasswd
+    wait
+
+    # スーパーユーザーかどうか
+    echo "sudo is $3"
+    if [[ $3 == "yes" ]]; then
+        usermod -aG sudo $1
+    fi
+    wait
+    echo "user '$1' is added"
+
+    # Shift all the parameters down by three
+    shift 3
+done
+
+echo -e "starting xrdp services...\n"
+
+trap "stop_xrdp_services" SIGKILL SIGTERM SIGHUP SIGINT EXIT
+start_xrdp_services
