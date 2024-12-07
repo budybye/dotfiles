2494aea3af0e25b7ab11bfab18d0859da01a66f7
--global <shallot96@gmail.com>
2024-12-07 18:15:50 +0900

    dda

--- install.sh
+++ install.sh
@@ -21,39 +21,12 @@ fi
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
 echo "SCRIPT_DIR: $SCRIPT_DIR"
 
-echo "$PATH" | grep -q "$BIN" || echo "export PATH=\"$BIN:\$PATH\"" >> "$SCRIPT_DIR/.env"
-echo "${PATH}" | tr ':' '\n'
-
-# ARCH 環境変数を設定
-if [ ! -f "$SCRIPT_DIR/.env" ]; then
-
-    echo "No .env file found."
-    if [ "$(command -v dpkg)" ]; then
-        echo "export ARCH=$(dpkg --print-architecture)" > "$SCRIPT_DIR/.env"
-
-    elif [ "$(command -v uname)" ]; then
-        echo "export ARCH=$(uname -m)" > "$SCRIPT_DIR/.env"
-    else
-        echo "ARCH is not detected."
-    fi
-
-    if [ "$ARCH" = "x86_64" ]; then
-        echo "export ARCH=amd64" > "$SCRIPT_DIR/.env"
-    fi
-fi
-cat "$SCRIPT_DIR/.env"
-echo "ARCH: $ARCH"
-
 if [ -z "$BW_SESSION" ]; then
     echo "BW_SESSION is not set."
 fi
 
 if [ -z "$GITHUB_TOKEN" ]; then
     echo "GITHUB_TOKEN is not set."
-fi
-
-if [ ! -f "$SCRIPT_DIR/home/key.txt.age" ]; then
-    echo "age is not set."
 fi
 
 # exec: replace current process with chezmoi init
