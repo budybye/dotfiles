8c2607fcce8633e65aa86bfbfca15a4f0039b5de
--global <shallot96@gmail.com>
2024-12-07 17:41:02 +0900

    ops

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -1,33 +1,40 @@
 #!/bin/bash
 
+# ユーザーを作成して xrdp サービスを開始する
+# 引数
+# $1: ユーザー名
+# $2: パスワード
+# $3: スーパーユーザーかどうか yes or other
+
+# root ユーザーで実行された場合は sudo をつけない
+sudo="sudo"
+if [ $(whoami) == "root" ]; then
+    echo "running root user"
+    sudo=""
+fi
+
 # xrdp サービスを開始
 start_xrdp_services() {
     # Preventing xrdp startup failure
-    sudo rm -rf /var/run/xrdp-sesman.pid
-    sudo rm -rf /var/run/xrdp.pid
-    sudo rm -rf /var/run/xrdp/xrdp-sesman.pid
-    sudo rm -rf /var/run/xrdp/xrdp.pid
+    $sudo rm -rf /var/run/xrdp-sesman.pid
+    $sudo rm -rf /var/run/xrdp.pid
+    $sudo rm -rf /var/run/xrdp/xrdp-sesman.pid
+    $sudo rm -rf /var/run/xrdp/xrdp.pid
 
     # Use exec ... to forward SIGNAL to child processes
-    sudo xrdp-sesman && exec sudo xrdp -n
+    $sudo xrdp-sesman && exec $sudo xrdp -n
 }
 
 # error が発生した場合に xrdp サービスを停止
 stop_xrdp_services() {
-    xrdp --kill
-    xrdp-sesman --kill
+    $sudo xrdp --kill
+    $sudo xrdp-sesman --kill
     exit 0
 }
 
 # エントリーポイントスクリプトが実行されたことを通知
 echo Entryponit script is Running...
 echo
-
-# cmd の引数を3つ指定 ユーザー名 パスワード スーパーユーザーかどうか
-# $1: ユーザー名
-# $2: パスワード
-# $3: スーパーユーザーかどうか yes or other
-# デフォルトは CMD["ubuntu", "ubuntu", "no"]
 
 # 引数の数を3で割った商をusersに格納
 users=$(($#/3))
@@ -55,33 +62,31 @@ fi
 while [ $# -ne 0 ]; do
 
     # グループを作成 ユーザー名と同じグループ名
-    sudo addgroup $1
-    echo "username is $1"
-    # useradd -m -s /bin/bash -g $1 $1
+    # echo "username is $1"
+    $sudo addgroup $1
     if [ $(command -v zsh) ]; then
-        sudo useradd -m -s /usr/bin/zsh -g $1 $1
+        $sudo useradd -m -s $(command -v zsh) -g $1 $1
     else
-        sudo useradd -m -s /bin/bash -g $1 $1
+        $sudo useradd -m -s /bin/bash -g $1 $1
     fi
+    wait
 
     # パスワードを設定
     # getent passwd | grep $1
-    echo $1:$2 | sudo chpasswd
+    echo $1:$2 | $sudo chpasswd
     wait
 
     # スーパーユーザーかどうか
-    echo "sudo is $3"
+    # echo "sudo is $3"
     if [[ $3 == "yes" ]]; then
-        sudo usermod -aG sudo $1
+        $sudo usermod -aG sudo $1
     fi
     wait
-    echo "user '$1' is added"
+    # echo "user '$1' is added"
 
     # Shift all the parameters down by three
     shift 3
 done
-
-export HOME=/home/$1
 
 echo -e "starting xrdp services...\n"
 
--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 set -e # -e: exit on error
 
@@ -6,7 +6,7 @@ if [ ! "$(command -v chezmoi)" ]; then
     BIN="$HOME/.local/bin"
     CHEZMOI="$BIN/chezmoi"
     if [ "$(command -v curl)" ]; then
-    sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$BIN"
+        sh -c "$(curl -fsSL https://git.io/chezmoi)" -- -b "$BIN"
     elif [ "$(command -v wget)" ]; then
         sh -c "$(wget -qO- https://git.io/chezmoi)" -- -b "$BIN"
     else
@@ -19,20 +19,42 @@ fi
 
 # POSIX way to get script's dir: https://stackoverflow.com/a/29834779/12156188
 SCRIPT_DIR="$(cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P)"
+echo "SCRIPT_DIR: $SCRIPT_DIR"
+
+echo "$PATH" | grep -q "$BIN" || echo "export PATH=\"$BIN:\$PATH\"" >> "$SCRIPT_DIR/.env"
+echo "$PATH" | grep 
 
 # ARCH 環境変数を設定
-if [ ! -f "$SCRIPT_DIR/home/.env" ]; then
+if [ ! -f "$SCRIPT_DIR/.env" ]; then
+
     echo "No .env file found."
-    if [ "$(command -v uname)" ]; then
-        echo "export ARCH=$(uname -m)" >> "$SCRIPT_DIR/home/.env"
-    elif [ "$(command -v dpkg)" ]; then
-        echo "export ARCH=$(dpkg --print-architecture)" >> "$SCRIPT_DIR/home/.env"
+    if [ "$(command -v dpkg)" ]; then
+        echo "export ARCH=$(dpkg --print-architecture)" > "$SCRIPT_DIR/.env"
+
+    elif [ "$(command -v uname)" ]; then
+        echo "export ARCH=$(uname -m)" > "$SCRIPT_DIR/.env"
     else
         echo "ARCH is not detected."
     fi
+
+    if [ "$ARCH" = "x86_64" ]; then
+        echo "export ARCH=amd64" > "$SCRIPT_DIR/.env"
+    fi
 fi
-cat "$SCRIPT_DIR/home/.env"
-. "$SCRIPT_DIR/home/.env"
+cat "$SCRIPT_DIR/.env"
+echo "ARCH: $ARCH"
+
+if [ -z "$BW_SESSION" ]; then
+    echo "BW_SESSION is not set."
+fi
+
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "GITHUB_TOKEN is not set."
+fi
+
+if [ ! -f "$SCRIPT_DIR/home/key.txt.age" ]; then
+    echo "age is not set."
+fi
 
 # exec: replace current process with chezmoi init
 exec "$CHEZMOI" init --apply "--source=$SCRIPT_DIR"
