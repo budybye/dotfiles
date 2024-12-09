ebc255800481faa109e702eb56fa07ce0f5ebdbc
--global <shallot96@gmail.com>
2024-12-10 03:45:48 +0900

    w

--- home/.chezmoiscripts/run_after_xrp.sh
+++ home/.chezmoiscripts/run_after_xrp.sh
@@ -36,12 +36,6 @@ USD_XRP_QUALITY=$(curl -s -X POST "$XRPL_ENDPOINT" \
     -d "$GET_USD_OFFERS" | \
     jq -r '.result.offers[].quality | (tonumber * 1000000)')
 
-# LedgerIndex を取得
-LEDGER_INDEX=$(curl -s -X POST "$XRPL_ENDPOINT" \
-    -H "Content-Type: application/json" \
-    -d "$GET_FEE" | \
-    jq -r '.result.ledger_current_index')
-
 # fee リクエストを一度だけ実行し、必要な情報を取得
 FEE_RESPONSE=$(curl -s -X POST "$XRPL_ENDPOINT" \
     -H "Content-Type: application/json" \
@@ -54,6 +48,6 @@ LEDGER_INDEX=$(echo "$FEE_RESPONSE" | jq -r '.result.ledger_current_index')
 OPEN_LEDGER_FEE=$(echo "$FEE_RESPONSE" | jq -r '.result.drops.open_ledger_fee')
 
 # 結果を表示
-echo "XRP/USD: $USD_XRP_QUALITY"
+echo "XRP/USD: \$$USD_XRP_QUALITY"
 echo "LedgerIndex: $LEDGER_INDEX"
 echo "Fee: $OPEN_LEDGER_FEE"
