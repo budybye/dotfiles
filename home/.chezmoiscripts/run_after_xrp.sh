429f28425e756dfd5e9bdd765a8f5a5b4a4fe481
--global <shallot96@gmail.com>
2025-05-07 08:48:32 +0900

    ii

--- home/.chezmoiscripts/run_after_xrp.sh
+++ home/.chezmoiscripts/run_after_xrp.sh
@@ -55,26 +55,23 @@ ACCOUNT_INFO_RESPONSE=$(grpc_request "$GET_ACCOUNT_INFO")
 if command -v jq > /dev/null 2>&1 ; then
     # get quality and multiply by 1,000,000
     XRP_CURRENCY_QUALITY=$(echo "$OFFER_RESPONSE" | jq -r '.result.offers[].quality | (tonumber * 1000000)')
-
     # get ledger_current_index
     LEDGER_INDEX=$(echo "$FEE_RESPONSE" | jq -r '.result.ledger_current_index')
-
     # get open_ledger_fee
     OPEN_LEDGER_FEE=$(echo "$FEE_RESPONSE" | jq -r '.result.drops.open_ledger_fee')
-
     # get balance
     ACCOUNT_BALANCE=$(echo "$ACCOUNT_INFO_RESPONSE" | jq -r '.result.account_data.Balance | (tonumber / 1000000)')
+
+    # json を整形して表示
+    # echo "$FEE_RESPONSE" | jq '.result'
+    # echo "$OFFER_RESPONSE" | jq '.result'
+    # echo "$ACCOUNT_INFO_RESPONSE" | jq '.result'
 
 else
     echo "jq is not installed"
     exit
 fi
 
-# echo "$FEE_RESPONSE" | jq '.result'
-# echo "$OFFER_RESPONSE" | jq '.result'
-# echo "$ACCOUNT_INFO_RESPONSE" | jq '.result'
-
-# show price, index, fee, account, balance
 echo "__  ______  ____    _             _"
 echo "\ \/ /  _ \|  _ \  | |    ___  __| | __ _  ___ _ __"
 echo " \  /| |_) | |_) | | |   / _ \/ _\` |/ _\` |/ _ \ '__|"
@@ -88,4 +85,4 @@ echo "Fee: $OPEN_LEDGER_FEE drops"
 echo "XRP/$CURRENCY: \$$XRP_CURRENCY_QUALITY"
 echo "Account: $ACCOUNT"
 echo "Balance: $ACCOUNT_BALANCE XRP"
-# echo "Time: $(date +%Y%m%d%H%M%S)"
+echo "$(date +%Y%m%dT%H%M%SZ)"
