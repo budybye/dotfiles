fa6a1ee25aa50393d86f6162019b55ade202aaf3
--global <shallot96@gmail.com>
2025-07-16 10:47:47 +0900

    😭

--- .devcontainer/entrypoint.sh
+++ .devcontainer/entrypoint.sh
@@ -90,7 +90,7 @@ while [ $# -ne 0 ]; do
 done
 
 echo -e "starting xrdp services...\n"
-echo "PORT is ${PORT:-3389}"
+echo "RDP_PORT is ${RDP_PORT:-3389}"
 
 trap "stop_xrdp_services" SIGKILL SIGTERM SIGHUP SIGINT EXIT
 start_xrdp_services
