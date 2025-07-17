281bb4a824357e7a5c59a994ad24b71b94dd9382
--global <shallot96@gmail.com>
2025-07-17 12:34:37 +0900

    🍆

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ DOTFILES=https://github.com/${GH_USER}/${GH_REPO}
 
 # chezmoi がインストールされていない場合はインストール
 if [ ! "$(command -v chezmoi)" ]; then
-    BIN="/usr/local/bin"
+    BIN="${HOME}/.local/bin"
     CHEZMOI="$BIN/chezmoi"
     # curl か wget がインストールされている場合は chezmoi をインストール
     if [ "$(command -v curl)" ]; then
